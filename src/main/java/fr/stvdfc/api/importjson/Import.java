ff36bf68cb7d0ab639a253eb495b96383e106a8a
Blackcat <56038765+baptiste1403@users.noreply.github.com>
2024-01-16 11:56:47 +0100

    Api 2 (#5)
    * [BAS] [#2] setup import of hashmap
    
    * [BAS] [#2] implementing the routes for the api
    
    * Add CrossOrigin annotation to BrowserController
    
    * Refactor Import class and update hashmap.json
    
    ---------
    
    Co-authored-by: Baptiste ASSELIN <baptiste.asselin@etu.univ-tours>
    Co-authored-by: Théo Souchon <60360657+TheoSouchon@users.noreply.github.com>

--- src/main/java/fr/stvdfc/api/importjson/Import.java
+++ src/main/java/fr/stvdfc/api/importjson/Import.java
@@ -14,11 +14,13 @@ import fr.stvdfc.api.interfaces.FileMetadataWithScore;
 import fr.stvdfc.api.models.KeywordSearchHashMap;
 import fr.stvdfc.api.models.SimpleFileMetadataWithScore;
 
-
-
 public class Import {
 
-    public static KeywordSearchHashMap importHashMapFromJson(String path){
+    private Import() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static KeywordSearchHashMap importHashMapFromJson(String path) {
 
         JSONArray array = null;
         try (InputStream fileReader = new FileInputStream(path)) {
@@ -27,34 +29,39 @@ public class Import {
             array = obj.getJSONArray("hashObject");
         } catch (IOException e) {
             e.printStackTrace();
-  
+            throw new NullPointerException("Error while reading JSON file");
         }
         KeywordSearchHashMap hashMap = new KeywordSearchHashMap();
-        for (int i=0; i < array.length(); i++){
+        for (int i = 0; i < array.length(); i++) {
             JSONObject obj = array.getJSONObject(i);
             JSONArray keywordsArray = obj.getJSONArray("keywords");
             SortedSet<String> keywords = getKeywordsFromJSON(keywordsArray);
             JSONArray resultArray = obj.getJSONArray("results");
-            for(int j=0 ; j < resultArray.length(); j++){
+            for (int j = 0; j < resultArray.length(); j++) {
                 FileMetadataWithScore fileMetadata = getFileMetadataFromJSON(resultArray.getJSONObject(j));
-                hashMap.add(keywords,fileMetadata);
+                hashMap.add(keywords, fileMetadata);
+
             }
         }
         return hashMap;
     }
-    private static SortedSet<String> getKeywordsFromJSON(JSONArray array){
+
+    private static SortedSet<String> getKeywordsFromJSON(JSONArray array) {
         SortedSet<String> keywords = new TreeSet<>();
-        for(int i = 0; i < array.length(); i++){
+        for (int i = 0; i < array.length(); i++) {
             String keyword = array.getString(i);
             keywords.add(keyword);
         }
         return keywords;
     }
-    private static FileMetadataWithScore getFileMetadataFromJSON(JSONObject obj){
+    private static FileMetadataWithScore getFileMetadataFromJSON(JSONObject obj) {
         SimpleFileMetadataWithScore file = new SimpleFileMetadataWithScore();
         file.setScore(obj.getInt("score"));
         JSONObject metadata = obj.getJSONObject("metadata");
-        file.addMetadata(metadata.getString("hashkey"), metadata.getString("value"));
+        for (String key : metadata.keySet()) {
+            file.addMetadata(key, metadata.getString(key));
+        }
         return file;
     }
 }
+
